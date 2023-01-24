e4ee844526c3629dbbc5ea68d54a839d5b467192
suramya-biswas-git <suramya.biswas@gmail.com>
2023-01-24 16:10:04 +0530

    First Version

---
+++ jenkinsAutoCommit/src/main/java/com/sb/jenkinsAutoCommit/App.java
@@ -0,0 +1,13 @@
+package com.sb.jenkinsAutoCommit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hi World!" );
+    }
+}
